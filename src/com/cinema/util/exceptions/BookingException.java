e184939da03a4dcefa6182c730447adf194dc508
Alex_H <ewebotah@gmail.com>
2025-04-20 10:52:14 +0200

    Added class BookingException
    ✅ Это исключение должно выбрасываться, когда не удается забронировать билет (например, если нет мест или сеанс уже начался).

---
+++ src/com/cinema/util/exceptions/BookingException.java
@@ -0,0 +1,10 @@
+package com.cinema.util.exceptions;
+
+/**
+ * Исключение, выбрасываемое при ошибке бронирования билета.
+ */
+public class BookingException extends Exception {
+    public BookingException(String message) {
+        super(message);
+    }
+}
