ef743d82f1a05133f6705f6453a31ef6213a6e30
Alex_H <ewebotah@gmail.com>
2025-04-30 17:42:06 +0200

    Added class ProductNotFoundException
    ✅ Это исключение должно выбрасываться, когда продукт с заданным ID не найден.

--- src/com/cinema/util/exceptions/ProductNotFoundException.java
+++ src/com/cinema/util/exceptions/ProductNotFoundException.java
@@ -1,7 +1,10 @@
 package com.cinema.util.exceptions;
 
+/**
+ * Бросается, когда продукт с заданным ID не найден.
+ */
 public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
+    public ProductNotFoundException(Long productId) {
+        super("Product not found with ID: " + productId);
     }
 }
